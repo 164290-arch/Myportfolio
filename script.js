664c9a1e5117e33bb2975dcac560550a9ea82184
164290-arch <164290@wsk.ac.th>
2026-08-04 13:45:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const text =
+"Grade 12 Student | Python Developer | Web Developer";
 
+let index = 0;
+
+function type(){
+
+    if(index < text.length){
+
+        document.getElementById("typing").innerHTML += text.charAt(index);
+
+        index++;
+
+        setTimeout(type,70);
+
+    }
+
+}
+
+window.onload = type;
